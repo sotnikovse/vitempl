33aad8b415633dffd2c43bb2d8acc22a900d7ee5
Stepan Sotnikov <sotnikovse1207@gmail.com>
2023-10-29 14:19:16 +0900

    feat: vitest

---
+++ packages/vite-plugin-sfce/test/utils/kebabCase.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from 'vitest'
+import { kebabCase } from '../../src/utils/kebabCase'
+
+describe('kebabCase.ts', () => {
+  test('should convert camelCase to kebab-case', () => {
+    expect(kebabCase('camelCase')).toBe('camel-case')
+  })
+
+  test('should convert PascalCase to kebab-case', () => {
+    expect(kebabCase('PascalCase')).toBe('pascal-case')
+  })
+
+  test('should convert snake_case to kebab-case', () => {
+    const input = 'snake_case'
+    const result = 'snake-case'
+    expect(kebabCase(input)).toBe(result)
+    expect(kebabCase(input.toUpperCase())).toBe(result)
+  })
+})
