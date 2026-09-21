edfa4c1a87aa17ba978a7a9d42fe166eca690f79
Stepan Sotnikov <sotnikovse1207@gmail.com>
2026-09-21 10:54:47 +0900

    chore: fix release script, publish by package name

--- scripts/release.ts
+++ scripts/release.ts
@@ -4,6 +4,8 @@ import type { Options as ExecaOptions, ResultPromise } from "execa";
 import { execa } from "execa";
 import { generateChangelog, release } from "@vitejs/release-scripts";
 
+const SCOPE = "@vitempl";
+
 function run<EO extends ExecaOptions>(
   bin: string,
   args: string[],
@@ -14,15 +16,22 @@ function run<EO extends ExecaOptions>(
   return execa(bin, args, { stdio: "inherit", ...opts }) as any;
 }
 
+function getPkgDir(pkgName: string) {
+  if (pkgName.startsWith(`${SCOPE}/`)) {
+    return `packages/${pkgName.slice(SCOPE.length + 1)}`;
+  }
+  return `packages/${pkgName}`;
+}
+
 async function getLatestTag(pkgName: string): Promise<string> {
-  const pkgJson = JSON.parse(
-    await readFile(`packages/${pkgName}/package.json`, "utf-8"),
-  );
+  const pkgDir = getPkgDir(pkgName);
+  const pkgJson = JSON.parse(await readFile(`${pkgDir}/package.json`, "utf-8"));
   const version = pkgJson.version;
   return pkgName === "vitempl" ? `v${version}` : `${pkgName}@${version}`;
 }
 
 async function logRecentCommits(pkgName: string): Promise<void> {
+  const pkgDir = getPkgDir(pkgName);
   const tag = await getLatestTag(pkgName);
   if (!tag) return;
   const sha = await run("git", ["rev-list", "-n", "1", tag], {
@@ -37,20 +46,14 @@ async function logRecentCommits(pkgName: string): Promise<void> {
   );
   await run(
     "git",
-    [
-      "--no-pager",
-      "log",
-      `${sha}..HEAD`,
-      "--oneline",
-      "--",
-      `packages/${pkgName}`,
-    ],
+    ["--no-pager", "log", `${sha}..HEAD`, "--oneline", "--", `${pkgDir}`],
     { stdio: "inherit" },
   );
   console.log();
 }
 
 release({
+  org: "sotnikovse",
   repo: "vitempl",
   packages: ["vitempl", "vite-plugin-sfce", "@vitempl/eslint-plugin"],
   toTag: (pkg, version) =>
@@ -59,8 +62,9 @@ release({
   generateChangelog: async (pkgName) => {
     console.log(colors.cyan("\nGenerating changelog..."));
     await generateChangelog({
-      getPkgDir: () => `packages/${pkgName}`,
+      getPkgDir: () => getPkgDir(pkgName),
       tagPrefix: pkgName === "vitempl" ? undefined : `${pkgName}@`,
     });
   },
+  getPkgDir: getPkgDir,
 });
