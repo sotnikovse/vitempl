81a5fd024ecb1c3d45b416e3d7a0fac15d92b0a1
Stepan Sotnikov <sotnikovse1207@gmail.com>
2026-09-21 11:44:32 +0900

    chore: @vitempl/eslint-plugin@0.1.0 alias

--- packages/eslint-plugin/tsdown.config.ts
+++ packages/eslint-plugin/tsdown.config.ts
@@ -1,7 +1,11 @@
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: "src/index.ts",
   dts: true,
   outExtensions: () => ({ js: ".js", dts: ".d.ts" }),
+  alias: {
+    vitempl: fileURLToPath(new URL("../vitempl/src/index.ts", import.meta.url)),
+  },
 });
